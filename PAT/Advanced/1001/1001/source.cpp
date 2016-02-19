43b9adef7c983251d3efae7f2391498e9faa379c
风之凌殇 <fzls.zju@gmail.com>
2016-02-19 12:45:30 +0800

    1001_before_refactor

---
+++ PAT/Advanced/1001/1001/source.cpp
@@ -0,0 +1,109 @@
+/*
++----------------------------------------------------------
+*
+* @authors: 风之凌殇 <fzls.zju@gmail.com>
+* @FILE NAME:    source.cpp
+* @version:      v1.0
+* @Time:         2016-02-19 11:35:04
+* @Description:  1001. A+B Format (20)
+时间限制
+400 ms
+内存限制
+65536 kB
+代码长度限制
+16000 B
+判题程序
+Standard
+作者
+CHEN, Yue
+Calculate a + b and output the sum in standard format -- that is, the digits must be separated into groups of three by commas (unless there are less than four digits).
+
+Input
+
+Each input file contains one test case. Each case contains a pair of integers a and b where -1000000 <= a, b <= 1000000. The numbers are separated by a space.
+
+Output
+
+For each test case, you should output the sum of a and b in one line. The sum must be written in the standard format.
+
+Sample Input
+-1000000 9
+Sample Output
+-999,991
+
+*
++----------------------------------------------------------
+*/
+#define _CRT_SECURE_NO_DEPRECATE
+#pragma comment(linker, "/STACK:66777216")
+
+#include <algorithm>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+#pragma region DebugSetting
+//#define DEBUG
+
+#ifdef DEBUG
+    #define debug(format, ...) printf("[line:%d:@%s] "format, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#else
+    #define debug(...)
+#endif
+#pragma endregion
+
+void print(int sum, int pos) {
+    if (sum == 0) {
+        return;
+    }
+
+    print(sum / 10, pos + 1);
+
+    if (pos % 3 == 0 && sum > 9) {
+        cout << ",";
+    }
+
+    cout << sum % 10;
+}
+
+void print(int sum) {
+    if(sum < 0) {
+        cout << "-";
+        sum = -sum;
+    }
+
+    if (sum == 0) {
+        cout << "0";
+    } else {
+        print(sum, 1);
+    }
+
+    cout << endl;
+}
+
+int main() {
+    #pragma region GET_INPUT
+    {
+        #ifndef ONLINE_JUDGE
+        freopen("test.in", "r", stdin);
+        freopen("test.out", "w", stdout);
+        #endif
+    }
+    #pragma endregion
+    int a, b;
+    cin >> a >> b;
+    print(a + b);
+    return 0;
+}
